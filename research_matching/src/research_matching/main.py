067f421cf1702b64eec12ab4dd0dcca0fce3785d
Frederic Laurin <frederic.3.laurin@gmail.com>
2025-01-15 15:26:35 +0700

    Initial commit without secrets

---
+++ research_matching/src/research_matching/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from research_matching.crew import ResearchMatchingCrew
+
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'company_website_url': 'https://neclab.eu/',
+        'publications_directory': 'https://mila.quebec/en/directory'
+    }
+    ResearchMatchingCrew().crew().kickoff(inputs=inputs)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: main.py run [<args>]")
+        sys.exit(1)
+
+    command = sys.argv[1]
+    if command == "run":
+        run()
+    else:
+        print(f"Unknown command: {command}")
+        sys.exit(1)
---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from research_matching.crew import ResearchMatchingCrew
+import openai
+
+# Just set the OpenAI API key directly
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+st.title("Research Matching App")
+
+company_website = st.text_input("Company Website URL", "https://neclab.eu/")
+publications_dir = st.text_input("Publications Directory", "https://mila.quebec/en/directory")
+
+if st.button("Run Matching"):
+    inputs = {
+        'company_website_url': company_website,
+        'publications_directory': publications_dir
+    }
+    crew = ResearchMatchingCrew()
+    results = crew.crew().kickoff(inputs=inputs)
+    st.write(results)
